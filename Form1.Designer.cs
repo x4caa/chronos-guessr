2594fbbb2bb09e423f2fbd40245ff6268656abbf
Jax <jaxbell37@gmail.com>
2023-08-25 16:42:01 -0700

    all ui elements size depending on screen size need to fix them resizing when form is resized

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.playButton = new System.Windows.Forms.Button();
+            this.leaderboardText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.Font = new System.Drawing.Font("Minecraft Ten", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.playButton.Location = new System.Drawing.Point(569, 275);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(75, 23);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "button1";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // leaderboardText
+            // 
+            this.leaderboardText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.leaderboardText.Location = new System.Drawing.Point(557, 327);
+            this.leaderboardText.Name = "leaderboardText";
+            this.leaderboardText.Size = new System.Drawing.Size(100, 16);
+            this.leaderboardText.TabIndex = 1;
+            this.leaderboardText.TextChanged += new System.EventHandler(this.leaderboardText_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.leaderboardText);
+            this.Controls.Add(this.playButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button playButton;
+        private TextBox leaderboardText;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,78 @@ namespace chronosguessr
         public Form1()
         {
             InitializeComponent();
+
+            //set fullscreen
+            this.Size = getScreenSize();
+            globalData.previousSize = this.Size;
+            this.WindowState = FormWindowState.Maximized;
+            //this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            PlayButtonInit();
+            LeaderboardTextInit();
+            Console.WriteLine("ABWHJDHJWABD");
+
+
+            this.Resize += MainForm_Resize;
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void leaderboardText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (this.Size != globalData.previousSize)
+            {
+                PlayButtonInit();
+                LeaderboardTextInit();
+                globalData.hi++;
+                leaderboardText.Text = $"FORM BEING RESIZED! {globalData.hi}";
+                Console.WriteLine("FORM IS BEING RESIZED!");
+            }
+
+            globalData.previousSize = this.Size;
+        }
+        private void PlayButtonInit()
+        {
+            Size screen = getScreenSize();
+            playButton.Text = "Play";
+            //font size is 50 on 1080p, 33 on 720p
+            playButton.Font = new Font("Minecraft Ten", (float)Math.Round(screen.Width / 38.4), FontStyle.Bold);
+            playButton.Size = new Size(screen.Width / 3, screen.Height / 8);
+
+            playButton.Location = new Point(screen.Width / 2 - playButton.Width / 2, screen.Height / 2 - playButton.Height / 2 - screen.Height / 10);
+        }
+
+        private void LeaderboardTextInit()
+        {
+            Size screen = getScreenSize();
+            leaderboardText.Multiline = true;
+            leaderboardText.ReadOnly = true;
+            leaderboardText.Size = new Size(playButton.Width, screen.Height / 5);
+
+            leaderboardText.Location = new Point(playButton.Location.X, playButton.Location.Y + playButton.Size.Height + screen.Height / 20);
+        }
+
+        private Size getScreenSize()
+        {
+            Screen mainScreen = Screen.PrimaryScreen;
+            return new Size(mainScreen.Bounds.Width, mainScreen.Bounds.Height);
+        }
+    }
+
+    static class globalData
+    {
+        public static Size previousSize;
+        public static int hi = 0;
     }
 }
